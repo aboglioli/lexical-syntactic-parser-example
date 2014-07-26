e975259372b24de68d2d73663bb545b16b8d9725
Alan <kiriost.s@gmail.com>
2014-07-25 22:01:30 -0300

    Proyecto casi listo.

--- compile_scanner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd src/
-flex declare_list.l
-gcc -o declare_list_scanner lex.yy.c -lfl
-mv declare_list_scanner ../bin/
--- test/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-bison -d test.y
-flex test.l
-gcc -o test test.tab.c lex.yy.c -lfl
